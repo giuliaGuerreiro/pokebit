151f96d6f0df536f6feca079bc0384cdde2b0f71
Giulia <giulia.guerreiro@gmail.com>
2025-04-15 02:31:47 -0300

    Home.tsx: Added PokemonList and Improved accessibility

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import { PokemonList } from '../components/PokemonList';
+
 const Home: React.FC = () => {
-  return <h1 className="font-retro text-xl">Pokémon List</h1>;
+  return (
+    <main role="main" className="px-2 py-4">
+      <h1 id="pokemon-heading" className="font-retro text-xl mb-4">
+        Pokémon List
+      </h1>
+
+      <section aria-labelledby="pokemon-heading" id="pokemon-list">
+        <PokemonList />
+      </section>
+    </main>
+  );
 };
 
 export default Home;

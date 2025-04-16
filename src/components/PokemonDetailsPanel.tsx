ca9ef8d76e5e58374e3a9a2bade7852cc5bf898b
Giulia <giulia.guerreiro@gmail.com>
2025-04-16 11:20:55 -0300

    PokemonDetailsPanel.tsx: Added animation

--- src/components/PokemonDetailsPanel.tsx
+++ src/components/PokemonDetailsPanel.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react';
 import { MobileDetailPanel } from './MobileDetailPanel';
 import { WebDetailPanel } from './WebDetailPanel';
+import { motion } from 'framer-motion';
 
-interface IWebDetailPanelProps {
+interface IDetailPanelProps {
   name: string;
   onClose: () => void;
 }
 
-export const PokemonDetailsPanel: React.FC<IWebDetailPanelProps> = ({ name, onClose }) => {
+export const PokemonDetailsPanel: React.FC<IDetailPanelProps> = ({ name, onClose }) => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -17,9 +18,17 @@ export const PokemonDetailsPanel: React.FC<IWebDetailPanelProps> = ({ name, onCl
     return () => window.removeEventListener('resize', check);
   }, []);
 
-  return isMobile ? (
-    <MobileDetailPanel name={name} onClose={onClose} />
-  ) : (
-    <WebDetailPanel name={name} onClose={onClose} />
+  return (
+    <motion.div
+      layout
+      className={isMobile ? 'fixed inset-0 z-50' : 'h-full'}
+      transition={{ duration: 0.4, ease: 'easeInOut' }}
+    >
+      {isMobile ? (
+        <MobileDetailPanel name={name} onClose={onClose} />
+      ) : (
+        <WebDetailPanel name={name} onClose={onClose} />
+      )}
+    </motion.div>
   );
 };

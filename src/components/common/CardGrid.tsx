704c627365ff0d98ef9a458a1db2bfefee42fe1c
Giulia <giulia.guerreiro@gmail.com>
2025-04-17 18:21:09 -0300

    CardGrid.tsx: Added skeleton and fixed cards layout

--- src/components/common/CardGrid.tsx
+++ src/components/common/CardGrid.tsx
@@ -4,45 +4,72 @@ import Button from './Button';
 type CardGridProps<T> = {
   items: T[];
   renderItem: (item: T, index: number) => React.ReactNode;
-  isLoading?: boolean;
+  isLoadingMore?: boolean;
+  isFirstLoad?: boolean;
   onLoadMore?: () => void;
   isDetailPanelOpen?: boolean;
 };
 
-// TODO: Add skeleton
-// TODO: Only show Load button when length > 0
-
+// TODO: Disable load more when all data loaded
 export function CardGrid<T extends { id?: string | number }>({
   items,
   renderItem,
-  isLoading,
+  isLoadingMore,
+  isFirstLoad,
   onLoadMore,
   isDetailPanelOpen = false,
 }: CardGridProps<T>) {
   const gridColumnsVariant = isDetailPanelOpen
-    ? 'grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4'
-    : 'grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4';
+    ? 'grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
+    : 'grid-cols-2 sm:grid-cols-3 md:grid-cols-4';
+
   return (
     <div className="h-full flex flex-col overflow-hidden">
-      <div role="list" className={`flex-1 overflow-y-auto pr-1 grid ${gridColumnsVariant}`}>
-        {items.map((item, index) => (
-          <div key={item.id ?? index} role="listitem">
-            {renderItem(item, index)}
-          </div>
-        ))}
-      </div>
+      {isFirstLoad ? (
+        <div
+          role="list"
+          className={`flex-1 overflow-y-auto pr-1 grid ${gridColumnsVariant} gap-4 auto-rows-max`}
+        >
+          {/* Skeleton */}
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <div
+                key={index}
+                className="cursor-default border rounded-xl shadow-sm p-4 bg-white border-gray-200 h-48"
+                data-testid="pokemon-card-skeleton"
+              >
+                <div className="w-32 h-32 mx-auto rounded-lg bg-gray-200 animate-pulse" />
+                <div className="h-5 mt-2 mx-auto w-20 bg-gray-200 rounded animate-pulse" />
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div
+          role="list"
+          className={`flex-1 overflow-y-auto pr-1 grid ${gridColumnsVariant} gap-4 auto-rows-max justify-items-center`}
+        >
+          {/* Cards */}
+          {items.map((item, index) => (
+            <div key={item.id ?? index} role="listitem" className="w-full max-w-md">
+              {renderItem(item, index)}
+            </div>
+          ))}
+        </div>
+      )}
 
-      {onLoadMore && (
+      {onLoadMore && items.length > 0 && (
         <div className="shrink-0 py-2">
           <Button
             onClick={onLoadMore}
             variant="primary"
             size="sm"
-            width="w-40"
-            isLoading={isLoading}
+            width="w-42"
+            isLoading={isLoadingMore}
             isCentralized
+            isDisabled={isFirstLoad}
           >
-            {isLoading ? 'Loading...' : 'Load More Pokemons'}
+            {isLoadingMore ? 'Loading...' : 'Load More Pokemons'}
           </Button>
         </div>
       )}

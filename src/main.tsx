df233d9c2bea99d8a022ebacf67f7067865a844e
Giulia <giulia.guerreiro@gmail.com>
2025-04-17 23:50:50 -0300

    main.tsx: Added HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
